35fc4cece102b4790919e88c3fc53af3a5cd1665
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2008-07-24 15:21:15 +0000

    moving from discriminant record types to oop to represent the values within the entity
    git-svn-id: http://dev.adaworks.net/svn/trunk/awent@450 115cb82a-aa27-0410-86a0-a2651309cc67

--- src/aw_ent.ads
+++ src/aw_ent.ads
@@ -76,56 +76,25 @@ package Aw_Ent is
 		);
 
 
-	type Value_Type_Record( Contains: Supported_Types ) is private;
-	-- The type Value_Type_Record is used to stored all information in a Ordered_Map.
+	type Value is abstract tagged null record;
 
-	type Value_Type is access all Value_Type_Record;
+	type Value_Ptr is access all Value;
 	-- this access type is used inside the map.
 	-- NOTE: There is no proper memory management implemented so far.
-	--       This should be provided by the Value_Type type, which
+	--       This should be provided by the Value_Ptr type, which
 	--      is to be changed to a controlled type soon.
 
-	------------------------
-	-- CONVERSION METHODS --
-	------------------------
-	
-	-- The following methods should be used to convert from your values
-	-- to Aw_Ent's values. These values are then mapped into a
-	--
-	-- Ordered_Map which is used by Aw_Ent to iterate with the database.
-
-	-- scalar types
-	function Get_Value( Value: APQ_Smallint ) return Value_Type;
-	function Get_Value( Value: APQ_Integer ) return Value_Type;
-	function Get_Value( Value: APQ_Bigint ) return Value_Type;
-	function Get_Value( Value: APQ_Real ) return Value_Type;
-	function Get_Value( Value: APQ_Double ) return Value_Type;
-	function Get_Value( Value: APQ_Serial ) return Value_Type;
-	function Get_Value( Value: APQ_Bigserial ) return Value_Type;
-
-	-- time types 
-	function Get_Value( Value: APQ_Date ) return Value_Type;
-	function Get_Value( Value: APQ_Time ) return Value_Type;
-	function Get_Value( Value: APQ_Timestamp ) return Value_Type;
-	function Get_Value( Value: APQ_Timezone ) return Value_Type;
-
-
-	-- other types
-	function Get_Value( Value: APQ_Boolean ) return Value_Type;
-	function Get_Value( Value: APQ_Bitstring ) return Value_Type;
-	function Get_Value( Value: String ) return Value_Type;
-	function Get_Value( Value: Unbounded_String ) return Value_Type;
 
 
 
 	package Valued_Maps is new Ada.Containers.Ordered_Maps(
 			Key_Type => Unbounded_String,
-			Element_Type => Value_Type );
+			Element_Type => Value_Ptr );
 
 	procedure Append(
 		Map: in out Valued_Maps.Map;
 		Key: in String;
-		Element: Value_Type );
+		Element: Value_Ptr );
 	-- this method is provided to easy the development, as it's not expected
 	-- to have the field names parammetrized.
 
@@ -134,7 +103,7 @@ package Aw_Ent is
 	-- The entity interface itself --
 	---------------------------------
 
-	type Entity_Interface is abstract tagged private;
+	type Entity is abstract tagged null record;
 	-- Every entity should implement the methods declared here.
 	--
 	-- Then the user will have authomatically some methods for
@@ -142,18 +111,18 @@ package Aw_Ent is
 	--
 
 
-	function Get_Keys( E: in Entity_Interface )
+	function Get_Keys( E: in Entity )
 		return Valued_Maps.Map is abstract;
-	procedure Set_Keys( E: in out Entity_Interface; Values: Valued_Maps.Map );
+	procedure Set_Keys( E: in out Entity; Values: Valued_Maps.Map );
 
 
-	function Get_Values( E: in Entity_Interface )
+	function Get_Values( E: in Entity )
 		return Valued_Maps.Map is abstract;
-	procedure Set_Values( E: in out Entity_Interface; Values: Valued_Maps.Map );
+	procedure Set_Values( E: in out Entity; Values: Valued_Maps.Map );
 
 
 	
-	function Get_Name( E: in Entity_Interface ) return String;
+	function Get_Name( E: in Entity ) return String;
 	-- Aw_Ent will use an active APQ connection and will expect the tables
 	--are named using the same name as the implementing type in UPERCASE.
 	-- Follow this rules and you'll be fine.
@@ -168,14 +137,14 @@ package Aw_Ent is
 	-- Entity Management Dispatching Methods --
 	-------------------------------------------
 	
-	procedure Load(	Entity_Object	: in out Entity_Interface'Class;
+	procedure Load(	Entity_Object	: in out Entity'Class;
 			Keys		: in Valued_Maps.Map );
 	-- load an entity using it's own allocated object
 	
-	procedure Store( Entity_Object : in Entity_Interface'Class );
+	procedure Store( Entity_Object : in Entity'Class );
 	-- save an already created entity in the backend
 	
-	procedure Insert( Entity_Object: in out Entity_Interface'CLass );
+	procedure Insert( Entity_Object: in out Entity'CLass );
 	-- insert a new value into the database backend.
 	-- Notice the entity is an "in out" parameter.
 	-- It's so it can be possible to Aw_Ent to determine auto-generated
@@ -183,66 +152,6 @@ package Aw_Ent is
 	
 
 
-private
-
-
-	type APQ_Bitstring_Access is access APQ_Bitstring;
-	-- this type is created so, internally, we can treat bitstring as it
-	-- was a constrained type.
-
-	type Value_Type_Record( Contains: Supported_Types ) is record
-		case Contains is
-			-- scalar types
-			when Type_APQ_Smallint	=>
-				APQ_Smallint_Value : APQ_Smallint;
-
-			when Type_APQ_Integer	=>
-				APQ_Integer_Value : APQ_Integer;
-
-			when Type_APQ_Bigint	=>
-				APQ_Bigint_Value : APQ_Bigint;
-			
-			when Type_APQ_Real	=>
-				APQ_Real_Value : APQ_Real;
-
-			when Type_APQ_Double	=>
-				APQ_Double_Value : APQ_Double;
-				
-			when Type_APQ_Serial	=>
-				APQ_Serial_Value : APQ_Serial;
-				
-			when Type_APQ_Bigserial	=>
-				APQ_Bigserial_Value : APQ_Bigserial;
-
-			-- time types
-			when Type_APQ_Date	=>
-				APQ_Date_Value : APQ_Date;
-
-			when Type_APQ_Time	=>
-				APQ_Time_Value : APQ_Time;
-			
-			when Type_APQ_Timestamp	=>
-				APQ_Timestamp_Value : APQ_Timestamp;
-
-			when Type_APQ_Timezone	=>
-				APQ_Timezone_Value : APQ_Timezone;
-			
-			-- other types.
-			when Type_APQ_Boolean	=>
-				APQ_Boolean_Value : APQ_Boolean;
-
-			when Type_APQ_Bitstring	=>
-				APQ_Bitstring_Value : APQ_Bitstring_Access;
-
-			when Type_String	=>
-				String_Value : Unbounded_String;
-		end case;
-	end record;
-
-
-
-	type Entity_Interface is abstract tagged null record;
-	-- the entity has no record here (for now. ;)).
 	
 
 end Aw_Ent;
