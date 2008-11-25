ef083ed0aee05c0aa487e266b9470264bcd9a8f1
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2008-11-25 02:14:26 +0000

    not quite there yet... I am having some problem in puting the final parts in the right places. It's gonna take some time but once it's done right I believe it's gonna be really straightforward to implement and use awent
    git-svn-id: http://dev.adaworks.net/svn/trunk/awent@590 115cb82a-aa27-0410-86a0-a2651309cc67

--- src/aw_ent.adb
+++ src/aw_ent.adb
@@ -4,7 +4,7 @@
 
 -- TODO: implement support for multiple database backends at the same time
 
-package Aw_Ent is
+package Aw_Ent.Prototype is
 
 	type Entity_Type is tagged private;
 	-- represents a data thats storedn into the database backend
@@ -25,26 +25,50 @@ package Aw_Ent is
 	procedure Load( Entity : in out Entity_Type; ID : in ID_Type );
 	-- load the entity from the default database Backend
 
-	procedure Load( Entity : in out Entity_Type; ID : in Natural );
-	-- load the entity from the default database Backend
-	-- it's the same as Load( Entity, To_ID( ID ) );
+	procedure Load( Entity : in out Entity_Type; ID : in Natural ) is
+		-- load the entity from the default database Backend
+		-- it's the same as Load( Entity, To_ID( ID ) );
+	begin
+		Entity.ID		:= To_ID( ID );
+		Entity.ID.My_Tag	:= Entity'Tag;
 	
-	procedure Store( Entity : in out Entity_Type; Recover_ID: Boolean := True );
-	-- save the entity into the database Backend
-	-- if it's a new entity, create a new entry and generates an ID for it.
-	-- If Recover_ID = TRUE then the ID is then loaded into the in-memory entity
-	-- after it has been saved.
-	
-	-- TODO: implement Narrow
-	-- procedure Narrow( From : in Entity_Type'Class; To: out Entity_Type'Class );
+		Load( Entity, Entity.ID );
+	end Load;
+
+
+	procedure Store( Entity : in out Entity_Type; Recover_ID: Boolean := True ) is
+		-- save the entity into the database Backend
+		-- if it's a new entity, create a new entry and generates an ID for it.
+		-- If Recover_ID = TRUE then the ID is then loaded into the in-memory entity
+		-- after it has been saved.
+
+		G : Entity_Getter_Type;
+	begin
+		for i in Properties( Entity ) loop
+			Append( Keys, Name( i ) );
+			G: Getter( i );
+			Append( Values, G( i ) );
+		end loop;
+
+		if E.ID.My_Tag = No_Tag then
+			Insert( E'Tag, Keys, Values, E.ID, Recover_ID );
+		else
+			Save( E'Tag, Keys, Values, E.ID );
+		end if;
+		
+		-- Construct_SQL( Table_Name( Entity'Tag ), Keys, Values );
+	end Store;
+
+
+	procedure Narrow( From : in Entity_Type'Class; To: out Entity_Type'Class );
 	-- narrow an entity to it's parent/child preserving/restoring properties
 	-- this is usefull for working with entities that extend a parent entity
 
 
 
-	--------------------------
-	-- Entity ID Management --
-	--------------------------
+	----------------
+	-- Entity IDs --
+	----------------
 
 
 
@@ -59,15 +83,30 @@ package Aw_Ent is
 	-- database backend.
 
 
-	----------------------------------
-	-- Entity Properties Management --
-	----------------------------------
+	-----------------------
+	-- Entity Properties --
+	-----------------------
 
 	type Entity_Property_Type is abstract tagged null record;
 
 	type Entity_Property_Ptr is access all Entity_Property_Type'Class;
 
+	procedure Set_Property(	Entity	: in out Entity_Type'Class;
+				Property: in Entity_Property_Type;
+				Results	: in APQ.Root_Query_Type'Class ) is abstract;
 
+	procedure Get_Property( Entity	: in Entity_Type'Class
+
+
+	type String_Getter_Type is access function(
+				Entity		: in Entity_Type'Class;
+				Property_Name	: in String
+			) return String;
+	type String_Setter_Type is access procedure(
+				Entity		: in out Entity_Type'Class,
+				Property_Name	: in String,
+				Value: in String );
+	
 	function New_String_Property(
 				Getter : String_Getter_Type;
 				Setter : String_Setter_Type
@@ -81,17 +120,17 @@ package Aw_Ent is
 
 
 	generic
-		type Property_Type is private;
+		type Value_Type is private;
 	package Generic_Property_Worker is
 		type Getter_Type is access function(
 				Entity		: in Entity_Type'Class,
 				Property_Name	: in String
-			) return Property_Type;
+			) return Value_Type;
 
 		type Setter_Type is access procedure(
 				Entity		: in out Entity_Type'Class,
 				Property_Name	: in String,
-				Value		: in Property_Type
+				Value		: in Value_Type
 			);
 
 		type Property_Type is new Entity_Property_Type with record
@@ -115,15 +154,8 @@ package Aw_Ent is
 
 
 	procedure Register(	Entity_Tag	: in Ada.Tags.Tag;
-				Table_Name	: in String;
 				Id_Generator	: in Id_Generator_Type := Standard_Id_Generator );
 	-- register an Entity into the Aw_Ent engine
-	-- Table_Name is the table name to be used.
-	
-	procedure Register(	Entity_Tag	: in Ada.Tags.Tag;
-				Id_Generator	: in Id_Generator_Type := Standard_Id_Generator );
-	-- register an Entity into the Aw_Ent engine
-	-- Auto generate the table name (using the Tag)
 
 
 
@@ -185,6 +217,7 @@ private
 			Element_Type	=> Property_Entry_Ptr
 			);
 
+
 	------------------------------------------------------------
 	-- Property Mapping is defined by using those types above --
 	------------------------------------------------------------
@@ -218,6 +251,3 @@ private
 	type Entity_Type is record
 		Original_Tag	: Ada.Tags.Tag;
 		-- this is queried internally by Narrow() and Store()
-
-
-end Aw_Ent;
