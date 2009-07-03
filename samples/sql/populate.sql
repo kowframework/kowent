4066cb1978e53db5041e64fed6983cae679e5662
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2009-07-03 14:16:40 +0000

    moving awent-samples to awent/samples
    git-svn-id: http://dev.adaworks.net/svn/trunk/awent@732 115cb82a-aa27-0410-86a0-a2651309cc67

---
+++ samples/sql/populate.sql
@@ -0,0 +1,12 @@
+use awent_samples;
+
+
+INSERT INTO books( author_id, title, extra_info ) VALUES ( 2, "Lol1", "lala lele aslçdjalkj" );
+INSERT INTO books( author_id, title, extra_info ) VALUES ( 2, "Lol2", "abc def ghi" );
+INSERT INTO books( author_id, title, extra_info ) VALUES ( 3, "Lol3", "123 456 789" );
+
+
+INSERT INTO authors( name ) VALUES( "OgRo" );
+INSERT INTO authors( name ) VALUES( "Marcelo" );
+INSERT INTO authors( name ) VALUES( "Dri" );
+
