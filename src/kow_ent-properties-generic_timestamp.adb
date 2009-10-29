9b1cf49e2425412e166117a702601270d0818bcd
Marcelo Coraça de Freitas <ogro@hank.(none)>
2009-10-29 17:50:15 -0200

    adding timestamp support

---
+++ src/kow_ent-properties-generic_timestamp.adb
@@ -0,0 +1,88 @@
+with APQ;
+
+
+package body KOW_ENT.Properties.Generic_Timestamp is
+
+
+	------------------
+	-- The Property --
+	------------------
+
+
+	overriding
+	procedure Set_Property(	
+				Property	: in     Property_Type;		-- the property worker
+				Entity		: in out Entity_Type'Class;		-- the entity
+				Q		: in out APQ.Root_Query_Type'Class;	-- the query from witch to fetch the result
+				Connection	: in out APQ.Root_Connection_Type'Class -- the connection that belongs the query
+			) is
+		-- Set the property from the query into the Entity.
+		Column : String := To_String( Property.Column_Name );
+		Index  : APQ.Column_Index_Type := APQ.Column_Index( Q, Column );
+	begin
+		Property.Setter.all(
+				Entity,
+				Value( Q, Index )
+			);
+		exception
+			when APQ.Null_Value =>
+				Property.Setter.all(
+						Entity,
+						Null_Value
+					);
+	end Set_Property;
+
+
+
+	overriding
+	procedure Get_Property(
+				Property	: in     Property_Type;		-- the property worker
+				Entity		: in     Entity_Type'Class;		-- the entity
+				Query		: in out APQ.Root_Query_Type'Class;	-- the query to witch append the value to insert
+				Connection	: in out APQ.Root_Connection_Type'Class	-- the connection that belongs the query
+			) is
+		-- Append into a query being created by the main KOW_ent engine.
+	begin
+		Append( Query, Property.Getter.all( Entity ) );
+	end Get_Property;
+
+
+
+	overriding
+	procedure Set_Property(
+				Property	: in     Property_Type;		-- the property worker
+				Entity		: in out Entity_Type'Class;		-- the entity
+				Value		: in     String				-- the String representation of this value
+			) is
+		-- Set the property from a String representation of the value
+	begin
+		Property.Setter.all( Entity, From_String( Value ) );
+	end Set_Property;
+
+
+
+	overriding
+	function Get_Property(
+				Property	: in     Property_Type;		-- the property worker
+				Entity		: in     Entity_Type'Class		-- the entity
+			) return String is
+	begin
+		return To_String( Property.Getter.all( Entity ) );
+	end Get_property;
+
+
+
+	function New_Property(
+					Column_Name	: in String;
+					Getter		: in Getter_Type;
+					Setter		: in Setter_Type
+			) return KOW_Ent.Entity_Property_Ptr is
+	begin
+		return new Property_Type'(
+					Column_Name	=> To_Unbounded_String( Column_Name ),
+					Getter		=> Getter,
+					Setter		=> Setter
+				);
+	end New_Property;
+
+end KOW_Ent.Properties.Generic_Timestamp;
