d4068ab27726e1ccfcdeab05bdb331edf1957bc4
Marcelo Coraça de Freitas <ogro@hank.(none)>
2009-11-19 20:03:41 -0200

    #28 - anonymous access types for entity properties :: generic range

--- src/kow_ent-properties-generic_range.adb
+++ src/kow_ent-properties-generic_range.adb
@@ -73,15 +73,15 @@ package body KOW_ENT.Properties.Generic_Range is
 
 	function New_Property(
 					Column_Name	: in String;
-					Getter		: in Getter_Type;
-					Setter		: in Setter_Type
+					Getter		: not null access function( Entity : in Entity_Type'Class ) return Val_Type;
+					Setter		: not null access procedure( Entity : in out Entity_Type'Class; Value : in Val_Type )
 			) return KOW_Ent.Entity_Property_Ptr is
+		PT : Property_Type;
 	begin
-		return new Property_Type'(
-					Column_Name	=> To_Unbounded_String( Column_Name ),
-					Getter		=> Getter,
-					Setter		=> Setter
-				);
+		PT.Column_Name	:= To_Unbounded_String( Column_Name );
+		PT.Getter	:= Getter;
+		PT.Setter	:= Setter;
+		return new Property_Type'( PT );
 	end New_Property;
 
 
