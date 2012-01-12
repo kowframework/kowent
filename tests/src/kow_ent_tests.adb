f6a68706303434341dd34195c1130359df23ab09
Marcelo C. de Freitas <marcelo@kow.com.br>
2012-01-12 00:09:00 -0200

    implemented tested for other types

--- tests/src/kow_ent_tests.adb
+++ tests/src/kow_ent_tests.adb
@@ -64,16 +64,41 @@ package body KOW_Ent_Tests is
 
 
 
+	My_Smallint_Name: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_smallint" );
 	My_Int_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_int" );
+	My_Bigint_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_bigint" );
 	My_Real_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_real" );
+	My_Double_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_double" );
+	My_Serial_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_serial" );
+	My_Bigserial_Name:constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_bigserial" );
+
+	-- TODO :: date, time and timestamp
+
+	My_Hour_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_hour" );
+	My_Minute_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_minute" );
+	My_Second_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_second" );
+
 	My_String_Name	: constant KOW_Ent.Property_Name_Type := KOW_Ent.PN( "my_string" );
 
 
 
 	-- test the core funcionality of properties -- how they hold value and the sorts
 	type My_Container is new KOW_Ent.Property_Container_Type with record
+		My_Smallint	: KOW_Ent.Properties.Smallint_Property( My_Smallint_Name, My_Container'Unrestricted_Access );
 		My_Int		: KOW_Ent.Properties.Integer_Property( My_Int_Name, My_Container'Unrestricted_Access );
+		My_Bigint	: KOW_Ent.Properties.Bigint_Property( My_Bigint_Name, My_Container'Unrestricted_Access );
 		My_Real		: KOW_Ent.Properties.Real_Property( My_Real_Name, My_Container'Unrestricted_Access );
+		My_Double	: KOW_Ent.Properties.Double_Property( My_Double_Name, My_Container'Unrestricted_Access );
+		My_Serial	: KOW_Ent.Properties.Serial_Property( My_Serial_Name, My_Container'Unrestricted_Access );
+		My_Bigserial	: KOW_Ent.Properties.Bigserial_Property( My_Bigserial_Name, My_Container'Unrestricted_Access );
+
+		-- TODO :: date, time and timestamp
+		
+
+		My_Hour		: KOW_Ent.Properties.Hour_Property( My_Hour_Name, My_Container'Unrestricted_Access );
+		My_Minute	: KOW_Ent.Properties.Minute_Property( My_Minute_Name, My_Container'Unrestricted_Access );
+		My_Second	: KOW_Ent.Properties.Second_Property( My_Second_Name, My_Container'Unrestricted_Access );
+
 		My_String	: KOW_Ent.Properties.String_Property( My_String_Name, My_Container'Unrestricted_Access, 2 );
 	end record;
 
@@ -89,26 +114,87 @@ package body KOW_Ent_Tests is
 			use KOW_Ent;
 		begin
 			Registered := True;
-			if P.Value_Of = KOW_Ent.APQ_Integer then
-				Ahven.Assert( P.Value.Integer_Value = 1, "integer not holding values" );
-				Ahven.Assert( P.Name.all = My_Int_Name.all, "integer not holding property name" );
 
-			elsif P.Value_Of = KOW_Ent.APQ_Real then
-				Ahven.Assert( P.Value.Real_Value = 20.0, "real not holding values" );
-				Ahven.Assert( P.Name.all = My_Real_Name.all, "real not holding property name" );
+			case P.Value_Of is
+				when KOW_Ent.APQ_Smallint =>
+					Ahven.Assert( P.Value.Smallint_Value = 1, "smallint not holding values" );
+					Ahven.Assert( P.Name.all = My_Smallint_Name.all, "smallint not holding property name" );
 
-			elsif P.Value_Of = KOW_Ent.APQ_String then
-				Ahven.Assert( P.Value.String_Value = "A ", "string not holding values" );
-				Ahven.Assert( P.Name.all = My_String_Name.all, "string not holding property name" );
-			else
-				Ahven.Assert( false, "not the right value_of" );
-			end if;
+				when KOW_Ent.APQ_Integer =>
+					Ahven.Assert( P.Value.Integer_Value = 1, "integer not holding values" );
+					Ahven.Assert( P.Name.all = My_Int_Name.all, "integer not holding property name" );
+
+				when KOW_Ent.APQ_Bigint =>
+					Ahven.Assert( P.Value.Bigint_Value = 1, "bigint not holding values" );
+					Ahven.Assert( P.Name.all = My_Bigint_Name.all, "bigint not holding property name" );
+
+				when KOW_Ent.APQ_Real =>
+					Ahven.Assert( P.Value.Real_Value = 20.0, "real not holding values" );
+					Ahven.Assert( P.Name.all = My_Real_Name.all, "real not holding property name" );
+
+				when KOW_Ent.APQ_Double =>
+					Ahven.Assert( P.Value.Double_Value = 20.0, "double not holding values" );
+					Ahven.Assert( P.Name.all = My_Double_Name.all, "double not holding property name" );
+
+				when KOW_Ent.APQ_Serial =>
+					Ahven.Assert( P.Value.Serial_Value = 1, "serial not holding values" );
+					Ahven.Assert( P.Name.all = My_Serial_Name.all, "serial not holding property name" );
+
+				when KOW_Ent.APQ_Bigserial =>
+					Ahven.Assert( P.Value.Bigserial_Value = 1, "bigserial not holding values" );
+					Ahven.Assert( P.Name.all = My_Bigserial_Name.all, "bigserial not holding property name" );
+
+
+
+				-- TODO :: implement those:
+				when KOW_Ent.APQ_Date =>
+					raise program_error with "not implemented";
+				when KOW_Ent.APQ_Time =>
+					raise program_error with "not implemented";
+				when KOW_Ent.APQ_Timestamp =>
+					raise program_error with "not implemented";
+
+
+				
+				when KOW_Ent.Hour_Number =>
+					Ahven.Assert( P.Value.Hour_Value = 1, "hour not holding values" );
+					Ahven.Assert( P.Name.all = My_Hour_Name.all, "hour not holding property name" );
+
+				when KOW_Ent.Minute_Number =>
+					Ahven.Assert( P.Value.Minute_Value = 1, "minute not holding values" );
+					Ahven.Assert( P.Name.all = My_Minute_Name.all, "minute not holding property name" );
+
+				when KOW_Ent.Second_Number =>
+					Ahven.Assert( P.Value.Second_Value = 1, "second not holding values" );
+					Ahven.Assert( P.Name.all = My_Second_Name.all, "second not holding property name" );
+
+
+
+
+				when KOW_Ent.APQ_String =>
+					Ahven.Assert( P.Value.String_Value = "A ", "string not holding values" );
+					Ahven.Assert( P.Name.all = My_String_Name.all, "string not holding property name" );
+			end case;
 		end Iterator;
 
 		My : My_Container;
 	begin
+		My.My_Smallint.Value.Smallint_Value := 1;
 		My.My_Int.Value.Integer_Value := 1;
+		My.My_Bigint.Value.Bigint_Value := 1;
 		My.My_Real.Value.Real_Value := 20.0;
+		My.My_Double.Value.Double_Value := 20.0;
+		My.My_Serial.Value.Serial_Value := 1;
+		My.My_Bigserial.Value.Bigserial_Value := 1;
+
+
+		-- TODO: date, time and timestamp
+
+		My.My_Hour.Value.Hour_Value := 1;
+		My.My_Minute.Value.Minute_Value := 1;
+		My.My_Second.Value.Second_Value := 1;
+
+
 		KOW_Ent.From_String( My.My_String.Value, "A" );
 
 		Iterate( My, Iterator'Access );
