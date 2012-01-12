60314ca176085e5101759d6189f6faab999ff897
Marcelo C. de Freitas <marcelo@kow.com.br>
2012-01-11 22:15:26 -0200

    adding makefile

---
+++ tests/src/tester.adb
@@ -0,0 +1,19 @@
+
+
+
+
+with Ahven.Test_Runner;
+with Ahven.Framework;
+
+with KOW_Ent_Tests;
+
+
+
+procedure Tester is
+	S : Ahven.Framework.Test_Suite_Access := Ahven.Framework.Create_Suite( "KOW_Ent" );
+begin
+
+	Ahven.Framework.Add_Test( S.all, new KOW_Ent_Tests.Test );
+	Ahven.Text_Runner.Run( S );
+	Ahven.Framework.Release_Suite( S );
+end Tester;
