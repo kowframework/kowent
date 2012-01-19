3e174dc172312d56a8fc465b99e9a9a0f9e4021d
Marcelo C. de Freitas <marcelo@kow.com.br>
2012-01-19 01:34:57 -0200

    adding package for database data storages

---
+++ src/kow_ent-db_data_storages.ads
@@ -0,0 +1,63 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                       KOW Framework :: Entities                          --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWLib is free software; you can redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWLib is distributed in the hope that it will be useful, but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWLib; see file COPYING.  If not, write --
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- Main KOW_Ent package.                                                    --
+--                                                                          --
+-- KOW_Ent is reponsible for handling persistent data in your application   --
+-- stored in Database backends using the native DB types.                   --
+------------------------------------------------------------------------------
+
+
+
+--------------
+-- Ada 2005 --
+--------------
+with Ada.Containers.Doubly_Linked_Lists;
+with Ada.Finalization;
+with Ada.Unchecked_Deallocation;
+
+-------------------
+-- KOW Framework --
+-------------------
+with APQ;
+with APQ_Provider;
+with KOW_Ent.Data_Storages;
+
+package KOW_Ent.DB_Data_Storages is
+
+	Provider : APQ_Provider.Connection_Provider_Ptr;
+
+
+	type Root_DB_Storage_Type is abstract new KOW_Ent.Data_Storages.Data_Storage_Type with null record;
+	-- this db storage type just implements the data storage
+
+end KOW_Ent.DB_Data_Storages;
