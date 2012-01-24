7d5b42ca6a8f18bff7ee1e73bbdfd3d5234a8c7b
Marcelo C. de Freitas <marcelo@kow.com.br>
2012-01-24 03:59:32 -0200

    some changes in the parallel insert code

--- example/src/mass_insert.adb
+++ example/src/mass_insert.adb
@@ -24,20 +24,39 @@ use users;
 
 
 procedure Mass_Insert is
+
+
+
+	procedure Insert is
+		U : User_Entity;
+	begin
+		KOW_Lib.String_Util.Copy( From => "Marcelo", To => U.Name.Value.String_Value );
+		Store( u );
+	end Insert;
+
+	task type insert_task;
+	task body insert_Task is
+	begin
+		for i in 1 .. 1_000 loop
+			Insert;
+		--	delay 0.1;
+		end loop;
+	end insert_Task;
+
+	type insert_ptr is access insert_Task;
+
+	t: insert_ptr;
+	
 begin
 	KOW_Config.Add_Config_Path( "." );
 	KOW_Ent.DB.Setup;
 
 	Ada.Text_IO.Put_Line( "Inserting tons of data... this will take some time (starting in 1 sec)" );
+
 	delay 1.0;
 
-	for i in 1 .. 10_000_000 loop
-		declare
-			U : User_Entity;
-		begin
-			KOW_Lib.String_Util.Copy( From => "Marcelo", To => U.Name.Value.String_Value );
-			Store( u );
-		end; 
+	for i in 1 .. 10 loop
+		T := new Insert_Task;
 	end loop;
 
 end Mass_Insert;
